63c50988ace0d1576a1c7ad3089a375245b1d09a
baconstarvation <ant.ucd@gmail.com>
2014-06-29 10:26:35 -0700

    on state modifiers

---
+++ 2-positioning_elements/0-positioning_elements.js
@@ -0,0 +1,13 @@
+var Engine = require('famous/core/Engine');
+var Surface = require('famous/core/Surface');
+
+var mainContext = Engine.createContext();
+
+var firstSurface = new Surface({
+	size: [100, 100],
+	properties: {
+		backgroundColor: '#FA5C4F'
+	}
+});
+
+mainContext.add(firstSurface);
