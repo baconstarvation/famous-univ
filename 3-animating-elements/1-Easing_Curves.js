18a34659fa1c4cb7efc0ebda9464bb8dc8502c09
baconstarvation <ant.ucd@gmail.com>
2014-07-03 08:24:16 -0700

    finished easing curves, now on Chaining Animations

--- 3-animating-elements/1-Easing_Curves.js
+++ 3-animating-elements/1-Easing_Curves.js
@@ -1,16 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
 var Engine = require('famous/core/Engine');
 var Surface = require('famous/core/Surface');
 var Transform = require('famous/core/Transform');
@@ -20,12 +7,12 @@ var Easing = require('famous/transitions/Easing');
 var mainContext = Engine.createContext();
 
 var surface = new Surface({
-  size: [100, 100],
-  properties: {
-    color: 'white',
-    textAlign: 'center',
-    backgroundColor: '#FA5C4F'
-  }
+	size: [100, 100],
+	properties: {
+		color: 'white',
+		textAlign: 'center',
+		backgroundColor: '#FA5C4F'
+	}
 });
 
 var stateModifier = new StateModifier();
@@ -33,6 +20,45 @@ var stateModifier = new StateModifier();
 mainContext.add(stateModifier).add(surface);
 
 stateModifier.setTransform(
-  Transform.translate(100, 300, 0),
-  { duration : 1000, curve: Easing.inOutBack }
+	Transform.translate(100, 300, 0),
+	{ duration : 1000, curve: Easing.inOutBack }
 );
+
+/*
+
+Easing Curves
+
+Famo.us has a library of over 30+ easing curves that can be accessed through our Easing methods. They're really fun to play with.
+
+inQuad
+outQuad
+inOutQuad
+inCubic
+outCubic
+inOutCubic
+inQuart
+outQuart
+inOutQuart
+inQuint
+outQuint
+inOutQuint
+inSine
+outSine
+inOutSine
+inExpo
+outExpo
+inOutExpo
+inCirc
+outCirc
+inOutCirc
+inElastic
+outElastic
+inOutElastic
+inBack
+outBack
+inOutBack
+inBounce
+outBounce
+inOutBounce
+
+*/
