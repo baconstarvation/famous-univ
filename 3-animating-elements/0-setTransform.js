43fa3458265dcc120717df9f452ccf1bdab1d7c9
baconstarvation <ant.ucd@gmail.com>
2014-07-03 08:17:51 -0700

    finished setTransform, now on Easing Curves

--- 3-animating-elements/0-setTransform.js
+++ 3-animating-elements/0-setTransform.js
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 var Engine = require('famous/core/Engine');
 var Surface = require('famous/core/Surface');
 var Transform = require('famous/core/Transform');
@@ -12,21 +6,23 @@ var StateModifier = require('famous/modifiers/StateModifier');
 var mainContext = Engine.createContext();
 
 var surface = new Surface({
-  size: [100, 100],
-  properties: {
-    color: 'white',
-    textAlign: 'center',
-    backgroundColor: '#FA5C4F'
-  }
+	size: [100, 100],
+	properties: {
+		color: 'white',
+		textAlign: 'center',
+		backgroundColor: '#FA5C4F'
+	}
 });
 
 var stateModifier = new StateModifier();
 
 mainContext.add(stateModifier).add(surface);
 
+// In the Positioning module, we pass in the transform during the instantiation of our state modifier.
+// We can also set the transform using the .setTransform() method.
 stateModifier.setTransform(
-  Transform.translate(100, 300, 0),
-  { duration : 1000, curve: 'easeInOut' }
+	// transition object:
+	Transform.translate(100, 300, 0), // defines the animation, how the transform should transition from its current value to the value specified.
+	// callback function:
+	{ duration : 1000, curve: 'easeInOut' } // Here we define the duration of the transition to be 1000 ms and the easing curve to be a standard ease in, ease out.
 );
-
-
