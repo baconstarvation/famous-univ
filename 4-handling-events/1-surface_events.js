8d791e211a71b71425790fa78e45b7ca782ff468
baconstarvation <ant.ucd@gmail.com>
2014-07-05 14:40:48 -0700

    finished setup. now working thru surface events

---
+++ 4-handling-events/1-surface_events.js
@@ -0,0 +1,22 @@
+var Engine = require('famous/core/Engine');
+var Surface = require('famous/core/Surface');
+
+var mainContext = Engine.createContext();
+
+var surface = new Surface({
+  size: [undefined, 100],
+  content: 'click me',
+  properties: {
+    color: 'white',
+    textAlign: 'center',
+    backgroundColor: '#FA5C4F'
+  }
+});
+
+mainContext.add(surface);
+
+surface.on('click', function() {
+  surface.setProperties({
+    backgroundColor: '#878785'
+  });
+});
---
+++ 4-handling-events/3-engine_events copy.js
@@ -0,0 +1,20 @@
+var Engine = require('famous/core/Engine');
+var Surface = require('famous/core/Surface');
+
+var mainContext = Engine.createContext();
+
+var surface = new Surface({
+  size: [undefined, 100],
+  content: 'resize your browser window',
+  properties: {
+    color: 'white',
+    textAlign: 'center',
+    backgroundColor: '#FA5C4F'
+  }
+});
+
+mainContext.add(surface);
+
+Engine.on('resize', function() {
+  surface.setContent('resized');
+});
