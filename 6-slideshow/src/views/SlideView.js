1001072f39fd72e1345307a620cf495100617c95
baconstarvation <ant.ucd@gmail.com>
2014-07-08 08:48:57 -0700

    on Create Background

--- 6-slideshow/src/views/SlideView.js
+++ 6-slideshow/src/views/SlideView.js
@@ -1,43 +1,44 @@
 /*** SlideView ***/
 
-// define this module in Require.JS
 define(function(require, exports, module) {
-
-		// Import additional modules to be used in this view
 		var View = require('famous/core/View');
 		var Surface = require('famous/core/Surface');
 		var Transform = require('famous/core/Transform');
 		var StateModifier = require('famous/modifiers/StateModifier');
 
-		// Constructor function for our SlideView class
 		function SlideView() {
-
-				// Applies View's constructor function to SlideView class
 				View.apply(this, arguments);
+
+				this.rootModifier = new StateModifier({
+						size: [400, 450]
+				});
+
+        // saving a reference to the new node
+        this.mainNode = this.add(this.rootModifier);
 		}
 
-		// Establishes prototype chain for SlideView class to inherit from View
 		SlideView.prototype = Object.create(View.prototype);
 		SlideView.prototype.constructor = SlideView;
 
-		// Default options for SlideView class
 		SlideView.DEFAULT_OPTIONS = {};
-
-		// Define your helper functions and prototype methods here
 
 		module.exports = SlideView;
 });
 
 
 
-// subclass of the view class & inherits its prototype objects & methods
+/*
 
+Although we've added multiple views to the render tree, if we check out browser,
+we won't actually see anything. That's because we haven't added any surfaces. Unlike
+surfaces, views don't actually map to DOM elements. They're empty render nodes with
+extra features.
 
-// contains the following:
+One feature views don't have by default is a size. To specify a size, we have to manually
+create a state modifier and add it to the view. Let's do that now for our slide view.
 
-// A render node that all the components in our view will attach to
+After adding the state modifier, we save a reference to the new render node.
+Everything added below this node will inherit the size of the modifier we just created.
+We will be adding this node as we create our surfaces.
 
-// Event input and output handlers
-
-// Options extended from the default options & the options passed in during
-// instantiation
+*/
