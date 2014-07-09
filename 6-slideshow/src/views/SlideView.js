8ff4b85e723d1801414e96d5a3cfb0645b685818
baconstarvation <ant.ucd@gmail.com>
2014-07-08 23:58:07 -0700

    finished 'create background', but wasnt rendering. now on refactoring

--- 6-slideshow/src/views/SlideView.js
+++ 6-slideshow/src/views/SlideView.js
@@ -1,44 +1,37 @@
 /*** SlideView ***/
 
 define(function(require, exports, module) {
-		var View = require('famous/core/View');
-		var Surface = require('famous/core/Surface');
-		var Transform = require('famous/core/Transform');
-		var StateModifier = require('famous/modifiers/StateModifier');
+    var View = require('famous/core/View');
+    var Surface = require('famous/core/Surface');
+    var Transform = require('famous/core/Transform');
+    var StateModifier = require('famous/modifiers/StateModifier');
 
-		function SlideView() {
-				View.apply(this, arguments);
+    // SlideView constructor function
+    // runs once for each new instance
+    function SlideView() {
+        View.apply(this, arguments);
 
-				this.rootModifier = new StateModifier({
-						size: [400, 450]
-				});
+        this.rootModifier = new StateModifier({
+            size: [400, 450]
+        });
 
-        // saving a reference to the new node
         this.mainNode = this.add(this.rootModifier);
-		}
 
-		SlideView.prototype = Object.create(View.prototype);
-		SlideView.prototype.constructor = SlideView;
+        var background = new Surface({
+               // undefined size will inherit size from parent modifier
+            properties: {
+                backgroundColor: '#FFFFF5',
+                boxShadow: '0 10px 20px -5px rgba(0, 0, 0, 0.5)'
+            }
+        });
 
-		SlideView.DEFAULT_OPTIONS = {};
+        this.mainNode.add(background);
+    }
 
-		module.exports = SlideView;
+    SlideView.prototype = Object.create(View.prototype);
+    SlideView.prototype.constructor = SlideView;
+
+    SlideView.DEFAULT_OPTIONS = {};
+
+    module.exports = SlideView;
 });
-
-
-
-/*
-
-Although we've added multiple views to the render tree, if we check out browser,
-we won't actually see anything. That's because we haven't added any surfaces. Unlike
-surfaces, views don't actually map to DOM elements. They're empty render nodes with
-extra features.
-
-One feature views don't have by default is a size. To specify a size, we have to manually
-create a state modifier and add it to the view. Let's do that now for our slide view.
-
-After adding the state modifier, we save a reference to the new render node.
-Everything added below this node will inherit the size of the modifier we just created.
-We will be adding this node as we create our surfaces.
-
-*/
