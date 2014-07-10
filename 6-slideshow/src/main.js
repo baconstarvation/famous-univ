68a7525adc6e0f4ff4e3d28e2b44f087f070b9ba
baconstarvation <ant.ucd@gmail.com>
2014-07-10 08:23:32 -0700

    added slide and slideshow views to main.js, now onto Refactor

--- 6-slideshow/src/main.js
+++ 6-slideshow/src/main.js
@@ -1,22 +1,35 @@
 /*** main.js ***/
 
 define(function(require, exports, module) {
-		var Engine = require('famous/core/Engine');
+    var Engine = require('famous/core/Engine');
+    
+  // import the AppView class using require
+    var AppView = require('views/AppView');
 
-		// import the AppView class using require
-		var AppView = require('views/AppView');
+  // import the SlideShowView class using require
+  	var SlideShowView = require('views/SlideShowView');
 
-		var mainContext = Engine.createContext();
+  // import the SlideView class using require
+  	var SlideView = require('views/SlideView');
 
-	// create a new instance of app view
-		var appView = new AppView();
+    var mainContext = Engine.createContext();
 
-	// create a new instance of our slideshow view
-		var slideshowView = SlideShowView();
+  // create a new instance of app view
+    var appView = new AppView();
 
+	// add the app view instance to the context
+    mainContext.add(appView);
 
-		// var slideView = SlideView();
+  // create a new instance of slideshow view
+  	var slideShowView = new SlideShowView();
 
-	// add the instance to the context
-		mainContext.add(appView);
+	// add the slide view instance to the context
+		mainContext.add(slideShowView);
+
+  // create a new instance of slide view
+  	var slideView = new SlideView();
+
+	// add the slide view instance to the context
+		mainContext.add(slideView);
+
 });
