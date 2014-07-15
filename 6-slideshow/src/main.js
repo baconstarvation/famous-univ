0ee5904492121b4e77b24c1bd5ffb0e6fa9f11f8
baconstarvation <ant.ucd@gmail.com>
2014-07-14 23:10:06 -0700

    on 'Passing Data'

--- 6-slideshow/src/main.js
+++ 6-slideshow/src/main.js
@@ -2,17 +2,38 @@
 
 define(function(require, exports, module) {
     var Engine = require('famous/core/Engine');
+
+  // import Utility
+    var Utility = require('famous/utilities/Utility');
     
   // import the AppView class using require
     var AppView = require('views/AppView');
 
+  // import SlideData
+    var SlideData = require('data/SlideData');
+
+    var mainContext = Engine.createContext();
+
+  // simple Get request to the Picasa api with callback
+    Utility.loadURL(SlideData.getUrl(), initApp);
+
+
+    function initApp(data) {
+      // parses out response data and retrieves array of urls
+        data = SlideData.parse(data);
+
+      // instantiates AppView with our url data
+        var appView = new AppView({ data : data });
+
+    }
+
+
+/*
   // import the SlideShowView class using require
   	var SlideShowView = require('views/SlideShowView');
 
   // import the SlideView class using require
   	var SlideView = require('views/SlideView');
-
-    var mainContext = Engine.createContext();
 
   // create a new instance of app view
     var appView = new AppView();
@@ -28,6 +49,7 @@ define(function(require, exports, module) {
 
   // create a new instance of slide view
   	var slideView = new SlideView();
+*/
 
 	// add the slide view instance to the context
 		mainContext.add(slideView);
