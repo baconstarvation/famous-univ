8333346597ac8dc0915952b65de1be7dc2e86939
baconstarvation <ant.ucd@gmail.com>
2014-08-03 14:16:54 -0700

    on Rotate and Skew

--- 10-timbre/start/src/views/StripView.js
+++ 10-timbre/start/src/views/StripView.js
@@ -8,12 +8,28 @@ define(function(require, exports, module) {
 
     function StripView() {
         View.apply(this, arguments);
+
+        _createBackground.call(this);
     }
 
     StripView.prototype = Object.create(View.prototype);
     StripView.prototype.constructor = StripView;
 
-    StripView.DEFAULT_OPTIONS = {};
+    StripView.DEFAULT_OPTIONS = {
+        width: 320,
+        height: 55
+    };
+
+    function _createBackground() {
+        var backgroundSurface = new Surface({
+            size: [this.options.width, this.options.height],
+            properties: {
+                backgroundColor: 'black'
+            }
+        });
+
+        this.add(backgroundSurface);
+    }
 
     module.exports = StripView;
 });
