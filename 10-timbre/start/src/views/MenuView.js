601d54b469ef8d8c85c9c6c7fb03390fdc30440a
baconstarvation <ant.ucd@gmail.com>
2014-08-03 15:28:36 -0700

    on Enable Swipe

--- 10-timbre/start/src/views/MenuView.js
+++ 10-timbre/start/src/views/MenuView.js
@@ -18,10 +18,32 @@ define(function(require, exports, module) {
     MenuView.prototype.constructor = MenuView;
 
     MenuView.DEFAULT_OPTIONS = {
-        stripData: {}
+        stripData: {},
+        topOffset: 37,
+        stripOffset: 58
     };
 
     function _createStripViews() {
+        this.stripModifiers = [];
+        var yOffset = this.options.topOffset;
+
+        for (var i = 0; i < this.options.stripData.length; i++) {
+            var stripView = new StripView({
+                iconUrl: this.options.stripData[i].iconUrl,
+                title: this.options.stripData[i].title
+            });
+
+            var stripModifier = new StateModifier({
+                transform: Transform.translate(0, yOffset, 0)
+            });
+
+            this.stripModifiers.push(stripModifier);
+            this.add(stripModifier).add(stripView);
+
+            yOffset += this.options.stripOffset;
+        }
+
+
         var stripView = new StripView();
         var stripModifier = new StateModifier({
             transform: Transform.translate(0, 200, 0)
@@ -29,6 +51,29 @@ define(function(require, exports, module) {
 
         this.add(stripModifier).add(stripView);
     }
+
+
+    function _createStripViews() {
+        this.stripModifiers = [];
+        var yOffset = this.options.topOffset;
+
+        for (var i = 0; i < this.options.stripData.length; i++) {
+            var stripView = new StripView({
+                iconUrl: this.options.stripData[i].iconUrl,
+                title: this.options.stripData[i].title
+            });
+
+            var stripModifier = new StateModifier({
+                transform: Transform.translate(0, yOffset, 0)
+            });
+
+            this.stripModifiers.push(stripModifier);
+            this.add(stripModifier).add(stripView);
+
+            yOffset += this.options.stripOffset;
+        }
+    }
+
 
     module.exports = MenuView;
 });
