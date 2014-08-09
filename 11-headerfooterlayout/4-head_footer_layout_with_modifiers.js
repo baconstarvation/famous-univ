0ef8c852087c77d55934b415c509e697ef605d36
baconstarvation <ant.ucd@gmail.com>
2014-08-09 16:22:58 -0700

    now on Grid Layout http://famo.us/university/layout/grid-layout/1/

---
+++ 11-headerfooterlayout/4-head_footer_layout_with_modifiers.js
@@ -0,0 +1,40 @@
+var Engine = require('famous/core/Engine');
+var Surface = require('famous/core/Surface');
+var HeaderFooterLayout = require("famous/views/HeaderFooterLayout");
+
+var mainContext = Engine.createContext();
+
+var layout = new HeaderFooterLayout({
+	headerSize: 100,
+	footerSize: 50
+});
+
+layout.header.add(new Surface({
+	content: "Header",
+	classes: ["red-bg"],
+	properties: {
+		textAlign: "center"
+	}
+}));
+
+layout.content.add(new Surface({
+	content: "Content",
+	classes: ["grey-bg"],
+	properties: {
+		textAlign: "center"
+	}
+}));
+
+layout.footer.add(new Surface({
+	content: "Footer",
+	classes: ["red-bg"],
+	properties: {
+		textAlign: "center"
+	}
+}));
+
+var stateModifier = new StateModifier({
+	size: [400, 400]
+});
+
+mainContext.add(stateModifier).add(layout);
